0ac071f66214125e08fd15ce2fab5cf7d5883ffc
栗山未来 <1780759836@qq.com>
2023-08-08 17:17:29 +0800

    新增bcrypt加密算法

--- common/tool/encryption.go
+++ common/tool/encryption.go
@@ -4,6 +4,8 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // MD5加密
@@ -19,4 +21,14 @@ func Md5ByString(str string) string {
 
 func Md5ByBytes(b []byte) string {
 	return fmt.Sprintf("%x", md5.Sum(b))
+}
+
+// bcrypt加密
+func BcryptByString(str string) string {
+	byteArr := []byte(str)
+	b, err := bcrypt.GenerateFromPassword(byteArr, 4)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
 }

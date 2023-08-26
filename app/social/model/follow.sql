640aa36f9a85ab214f0d917e2c8e226a5c1c7f82
栗山未来 <1780759836@qq.com>
2023-08-26 13:01:26 +0800

    修改follow表定义

--- app/social/model/follow.sql
+++ app/social/model/follow.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS `follow`;
 CREATE TABLE `follow`  (
-  `id` int(11) NOT NULL COMMENT '关注表的主键id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '关注表的主键id',
   `user_id` int(11) NOT NULL COMMENT '关注人的id',
   `to_user_id` int(11) NOT NULL COMMENT '被关注人id',
   `status` int(1) NOT NULL COMMENT '是否为互相关注 1为相互关注 0不是', 
@@ -10,4 +10,4 @@ CREATE TABLE `follow`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `user_id`(`user_id`) USING BTREE COMMENT '关注列表使用此索引',
   UNIQUE KEY `to_user_id`(`to_user_id`) USING BTREE COMMENT '粉丝列表使用此索引'
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
